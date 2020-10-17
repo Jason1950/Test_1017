fa22aab4a56927a4ac46aab175df039bbc17a2d8
Jason1950 <eric870615@yahoo.com.tw>
2020-10-17 15:48:33 +0800

    commit log : first time for update

---
+++ cam_test.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+
+
+while(True):
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow(frame)
+    else:
+        break
+
+#test
+#     
